71ec7f6c3e990fb264b28f66f74a78ce0e56ddb2
lucas <lucax.oliveira96@gmail.com>
2022-02-03 23:35:57 -0300

    Dockerfile atualizado e docker-compose criado. Obs: Não testei porque to com sono

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.10
 
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
 WORKDIR /papyroAPI
 
 COPY ./requirements.txt /papyroAPI/requirements.txt
@@ -8,4 +11,3 @@ RUN pip install --no-cache-dir --upgrade -r /papyroAPI/requirements.txt
 
 COPY ./src /papyroAPI/src
 
-CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "80"]
--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -1,9 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./papyroAPI.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+config = Config(".env")
+
+ECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
+POSTGRES_USER = config("POSTGRES_USER", cast=str)
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
+POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
+POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
+POSTGRES_DB = config("POSTGRES_DB", cast=str)
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./papyroAPI.db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
