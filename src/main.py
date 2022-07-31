0eb5831a031764418b80f0edd9d206888f1f99bc
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-07-31 11:13:41 -0300

    adjust refresh token

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_jwt_auth.exceptions import AuthJWTException
+
 from src.routers import router
 
 app = FastAPI()
 
-#CORS
-origins=['http://localhost:3000']
+# CORS
+origins = ['http://localhost:3000']
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -13,5 +16,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
 
 app.include_router(router.router)
--- src/routers/login.py
+++ src/routers/login.py
@@ -1,74 +1,69 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi_jwt_auth import AuthJWT
 from sqlalchemy.orm import Session
-from src.core import hash_provider, token_provider
-from fastapi.security import OAuth2PasswordRequestForm
 
-from src.schemas.login import Login,LoginSucesso
+from src.core import hash_provider
 from src.core.config import Settings
-from src.db.models.models import User
-from src.db.database import get_db
-from src.routers.login_utils import obter_usuario_logado
 from src.crud.usuario import CrudUsuario
-from src.schemas.usuario import UsuarioSimples 
+from src.db.database import get_db
+from src.schemas.login import Login, LoginSucesso
+from src.schemas.usuario import UsuarioSimples
 
 settings = Settings()
 router = APIRouter()
 
 
-@router.post("/"
-, response_model=LoginSucesso
-)
-def login(login: Login, session: Session = Depends(get_db)):
-	try:
-		if not login.email:
-			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-								detail="Preencha o E-mail.")
-		elif not login.password:
-			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-								detail="Preencha a Senha.")
+@AuthJWT.load_config
+def get_config():
+    return Settings()
 
 
-		user = CrudUsuario(session).buscar_por_email(login.email)
-		
-		if not user:
-			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-								detail="Usuário não cadastrado.")
-		
-		if user.active == False:
-			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-						detail="Por favor, ative sua conta.")
+@router.post("/", response_model=LoginSucesso)
+def login(login: Login, session: Session = Depends(get_db), Authorize: AuthJWT = Depends()):
+    if not login.email:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Preencha o E-mail.")
+    elif not login.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Preencha a Senha.")
 
-		senha_valida = hash_provider.verify_password(login.password, user.password)
+    user = CrudUsuario(session).buscar_por_email(login.email)
 
-		if not senha_valida:
-			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-								detail="Senha inválida.")
-		
-		access_token = token_provider.create_access_token({'sub': user.email},
-												expires_delta=settings.USER_TOKEN_LIFETIME )
-		
-		refresh_token = token_provider.create_refresh_token({'sub': user.email}, expires_delta=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuário não cadastrado.")
 
-		us = UsuarioSimples()
-		us.name=user.name
-		us.nickname=user.nickname
-		us.photo=user.photo
-		us.description=user.description
-		
-		lo = LoginSucesso()
-		lo.user=us
-		lo.access_token=access_token
-		lo.refresh_token=refresh_token
-		lo.token_type="Bearer"
-		return lo
-	except:
-		raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-	
+    if not user.active:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Por favor, ative sua conta.")
+
+    senha_valida = hash_provider.verify_password(login.password, user.password)
+
+    if not senha_valida:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Senha inválida.")
+
+    access_token = Authorize.create_access_token(subject=user.email, expires_time=settings.USER_TOKEN_LIFETIME)
+    refresh_token = Authorize.create_refresh_token(subject=user.email, expires_time=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    us = UsuarioSimples()
+    us.name = user.name
+    us.nickname = user.nickname
+    us.photo = user.photo
+    us.description = user.description
+
+    lo = LoginSucesso()
+    lo.user = us
+    lo.access_token = access_token
+    lo.refresh_token = refresh_token
+    lo.token_type = "Bearer"
+    return lo
+
 
 @router.post("/refresh")
-def refresh(current_user: User = Depends(obter_usuario_logado)):
-	
-	new_access_token = token_provider.create_access_token({'sub': current_user.email},
-												expires_delta=settings.USER_TOKEN_LIFETIME)
-	
-	return {"access_token": new_access_token, "token_type":"Bearer"}
+def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return {"access_token": new_access_token}
