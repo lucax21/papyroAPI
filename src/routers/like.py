5963d000868798f66cf141ba4d2693a40ffd8120
lucas S. Oliveira <lucax.oliveira96@gmail.com>
2022-08-06 16:49:25 -0300

    fix

--- src/routers/like.py
+++ src/routers/like.py
@@ -11,9 +11,9 @@ router = APIRouter()
 
 @router.patch("/{like_type}/{id}/{mode}")
 async def like(id: int, like_type: str, mode: bool,
-            #    current_user: Usuario = Depends(obter_usuario_logado),
+               current_user: Usuario = Depends(obter_usuario_logado),
                session: Session = Depends(get_db)):
     if like_type != 'r' and like_type != 'c':
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Argumento de tipo de post inválido.")
-# current_user.id
-    return CrudLike(session).like_by_id(id, like_type, 2, mode)
+
+    return CrudLike(session).like_by_id(id, like_type, current_user.id, mode)
