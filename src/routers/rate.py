76771ad4b6913e2dc1d09065d6d2c86cb45bf350
jj <lucax.oliveira96@gmail.com>
2022-08-12 19:56:59 -0300

    FRONT-163-rota-avaliar

--- src/routers/rate.py
+++ src/routers/rate.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
-from src.crud.rate import CrudAvaliacao
+from src.routers.login_utils import obter_usuario_logado
+from src.db.models.models import User
+from src.crud.rate import CrudRate
 from src.db.database import get_db
+from src.schemas.rate import NewRate
 
 router = APIRouter()
 
 
 @router.get("/{id}")
-def carregar_avaliacao(id: int, session: Session = Depends(get_db)):
-    return CrudAvaliacao(session).carregar_avaliacao(id)
+async def carregar_avaliacao(id: int, session: Session = Depends(get_db)):
+    return CrudRate(session).carregar_avaliacao(id)
+
+@router.post("/")
+async def new_rate(data: NewRate, session: Session = Depends(get_db), current_user: User = Depends(obter_usuario_logado)):
+    return CrudRate(session).new_rate(current_user.id, data)
