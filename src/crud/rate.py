6b1948c36de76e76198417987a956d9418ec2a0f
lucax21 <67073237+lucax21@users.noreply.github.com>
2022-08-12 21:38:03 -0300

    Update rate.py

--- src/crud/rate.py
+++ src/crud/rate.py
@@ -10,16 +10,6 @@ class CrudRate():
     def __init__(self, session: Session):
         self.session = session
 
-    def carregar_avaliacao(self, id: int):
-        query = self.session.query(models.Rate).options(joinedload(models.Rate.usuario),
-                                                        joinedload(models.Rate.avaliacao).options(
-                                                            joinedload(models.Book.test2)),
-                                                        joinedload(models.Rate.comentarios).options(
-                                                            joinedload(models.Like.comentario))) \
-            .where(models.Like.fk_avaliacao == models.Rate.id)
-        # .where(models.Avaliacao.id == id)
-        return query.first()
-
 
     def new_rate(self, id_user:int, data: NewRate):
         
