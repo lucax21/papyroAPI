908eaf46d1cbbc05d5e594d27c100839fe7fd911
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-08-13 16:19:03 -0300

    fix book

--- src/crud/book.py
+++ src/crud/book.py
@@ -74,7 +74,9 @@ class CrudBook:
             .first()
 
         people_reading = self.session.query(func.count(models.UserBook.fk_user).label('users')) \
-            .where((models.UserBook.fk_status == ReadingTypes.READING)).first()
+            .where(models.UserBook.fk_status == ReadingTypes.READING).group_by(models.UserBook.fk_book).first()
+
+        print(people_reading)
 
         book = get_by_identifier(data.identifier)
 
