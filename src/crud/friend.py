816be499db347071f4fd2bdd075afd722627732f
jj <lucax.oliveira96@gmail.com>
2022-08-14 14:58:36 -0300

    FRONT-172-rota-amigos

--- src/crud/friend.py
+++ src/crud/friend.py
@@ -1,4 +1,5 @@
-from sqlalchemy import and_
+from fastapi import HTTPException, status
+from sqlalchemy import and_, update
 from sqlalchemy.orm import Session
 from src.db.models import models
 
@@ -8,12 +9,32 @@ class CrudFriend:
         self.session = session
 
     def get_friends(self, id: int, page: int):
-        data = self.session.query(models.User.id, models.User.nickname, models.User.photo)\
+        return self.session.query(models.User.id, models.User.nickname, models.User.photo)\
                 .where(and_(models.Friend.fk_origin == id,
-                            #models.Friend.pending == False,
-                            #models.Friend.ignored == False
+                            models.Friend.pending == False,
+                            models.Friend.ignored == False
                             ))\
                 .join(models.Friend, models.User.id == models.Friend.fk_destiny)\
                 .offset(page * 8).limit(8).all()
-        return data
+    
+    def accept_or_block_friend(self, id_user, oper_type, mode, id_friend):
+        if oper_type == 'a':
+            update_friend = update(models.Friend).values(pending=mode).where(and_(models.Friend.fk_origin == id_user, models.Friend.fk_destiny == id_friend))
+            self.session.execute(update_friend)
+            self.session.commit()
+            return 1
+        if oper_type == 'i':
+            update_friend = update(models.Friend).values(ignored=mode).where(and_(models.Friend.fk_origin == id_user, models.Friend.fk_destiny == id_friend))
+            self.session.execute(update_friend)
+            self.session.commit()
+            return 1
 
+    def add_friend(self, id_user, id_friend):
+        try:
+            stmt = models.Friend(fk_origin=id_user, fk_destiny=id_friend, pending=True, ignored = False)
+            self.session.add(stmt)
+            self.session.commit()
+            return 1
+        except Exception:
+            self.session.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao adicionar amigo.")
