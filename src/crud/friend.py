5b3eefca47ad55fdac236fc2de9f77a6f36175f0
jj <lucax.oliveira96@gmail.com>
2022-08-16 20:54:46 -0300

    fix friend_type

--- src/crud/friend.py
+++ src/crud/friend.py
@@ -8,15 +8,27 @@ class CrudFriend:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_friends(self, id: int, page: int):
-        return self.session.query(models.User.id, models.User.nickname, models.User.photo)\
+    def get_friends(self, id: int, friend_type: str, page: int):
+        
+        if friend_type == "following":
+            return self.session.query(models.User.id, models.User.nickname, models.User.photo)\
                 .where(and_(models.Friend.fk_origin == id,
                             models.Friend.pending == False,
                             models.Friend.ignored == False
                             ))\
                 .join(models.Friend, models.User.id == models.Friend.fk_destiny)\
                 .offset(page * 16).limit(16).all()
+        if friend_type == "followers":
+            return self.session.query(models.User.id, models.User.nickname, models.User.photo)\
+                .where(and_(models.Friend.fk_destiny == id,
+                            models.Friend.pending == False,
+                            models.Friend.ignored == False
+                            ))\
+                .join(models.Friend, models.User.id == models.Friend.fk_origin)\
+                .offset(page * 16).limit(16).all()
     
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Passe o parâmetro de leitura.")
+
     def accept_or_ignored_friend(self, id_user, oper_type, mode, id_friend):
         if oper_type == 'a':
             update_friend = update(models.Friend).values(pending=mode).where(and_(models.Friend.fk_origin == id_friend, models.Friend.fk_destiny == id_user))
