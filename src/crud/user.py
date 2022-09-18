0c41c1f8f60ab0433c7d15ac848c609ff709b5e9
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-09-18 18:10:43 -0300

    fix suggestion people

--- src/crud/user.py
+++ src/crud/user.py
@@ -142,7 +142,7 @@ class CrudUser:
         FROM result
             JOIN "user" on result.suggestion = "user".id
         WHERE "user".id <> :x 
-            AND "user".id NOT IN (SELECT f.fk_destiny FROM friend f where f.fk_origin = id)
+            AND "user".id NOT IN (SELECT f.fk_destiny FROM friend f where f.fk_origin = :x)
         ORDER BY result.quantity DESC
         OFFSET :y
         LIMIT 20
